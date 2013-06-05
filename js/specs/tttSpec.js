6597546dd43a0859995140eb77edf32391291743
Trevor Murphy <murphyl337@gmail.com>
2013-06-05 12:59:21 -0400

    jasmine added

---
+++ js/specs/tttSpec.js
@@ -0,0 +1,12 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: tmurphy
+ * Date: 6/5/13
+ * Time: 12:53 PM
+ * To change this template use File | Settings | File Templates.
+ */
+describe("something", function(){
+    it("1 should equal 1", function(){
+        expect(1).toEqual(1);
+    });
+});
---
+++ js/tictactoe.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: tmurphy
+ * Date: 6/5/13
+ * Time: 12:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
