cef77e856130b14293152dd6b7fcd36f9ab899eb
Trevor Murphy <murphyl337@gmail.com>
2013-06-05 14:43:43 -0400

    changed spec names

--- js/specs/tttSpec.js
+++ js/specs/tttSpec.js
@@ -13,31 +13,31 @@ describe("Game board", function(){
         gameBoard.initialize();
     });
 
-    it("should be defined", function(){
+    it("is defined", function(){
         expect(gameBoard).toBeDefined();
     });
 
-    it("should have 9 spaces", function(){
+    it("has 9 spaces", function(){
         expect(gameBoard.spaces.length).toBe(9);
     });
 
-    it("should be blank when initialized", function(){
+    it("is blank when initialized", function(){
         for(var space=0; space<gameBoard.spaces.length; space++){
             expect(gameBoard.spaces[space].mark).toBe("-");
         }
     });
 
-    it("should change the mark on a space when updated", function(){
+    it("changes the mark on a space when updated", function(){
         gameBoard.updateBoard("X", 0);
         expect(gameBoard.spaces[0].mark).toBe("X");
     });
 
-    it("should return all spaces that haven't been taken", function(){
+    it("returns all spaces that haven't been taken", function(){
         var availableSpaces = gameBoard.getAvailableSpaces();
         expect(availableSpaces.length).toBe(9);
     });
 
-    it("should return a board with identical properties when cloned", function(){
+    it("returns a board with identical properties when cloned", function(){
         var clone = gameBoard.clone();
         for(var space=0; space<9; space++){
             expect(clone.spaces[space].mark).toBe(gameBoard.spaces[space].mark);
@@ -45,17 +45,17 @@ describe("Game board", function(){
         }
     });
 
-    it("should return a new object when cloned", function(){
+    it("returns a new object when cloned", function(){
         var clone = gameBoard.clone();
         expect(clone).not.toBe(gameBoard);
     });
 
     describe("Space", function(){
-        it("should have a mark field", function(){
+        it("has a mark field", function(){
             expect(gameBoard.spaces[0].mark).toBeDefined();
         });
 
-        it("should have a position field", function(){
+        it("has a position field", function(){
             expect(gameBoard.spaces[0].position).toBeDefined();
         });
     });
