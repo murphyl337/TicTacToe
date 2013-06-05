909eaeb42aec187107ab9ec11520c931fbd8ac8c
Trevor Murphy <murphyl337@gmail.com>
2013-06-05 13:08:58 -0400

    game board defined

--- js/specs/tttSpec.js
+++ js/specs/tttSpec.js
@@ -5,8 +5,13 @@
  * Time: 12:53 PM
  * To change this template use File | Settings | File Templates.
  */
-describe("something", function(){
-    it("1 should equal 1", function(){
-        expect(1).toEqual(1);
+describe("Game board", function(){
+    var gameBoard;
+    beforeEach(function(){
+        gameBoard = new GameBoard();
+    });
+
+    it("should have 9 spaces", function(){
+        expect(gameBoard.spaces.length).toEqual(9);
     });
 });
--- js/tictactoe.js
+++ js/tictactoe.js
@@ -5,3 +5,10 @@
  * Time: 12:44 PM
  * To change this template use File | Settings | File Templates.
  */
+function GameBoard(){
+    this.spaces = [];
+    var boardSize = 9;
+
+    for(var space=0; space<boardSize; space++)
+        this.spaces[space] = "";
+}
